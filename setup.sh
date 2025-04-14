debbfabc6280a522a4512e70c2e0b26b29fdd9be
kheiriddine <111566229+kheiriddine@users.noreply.github.com>
2025-04-14 23:40:15 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 apt-get update && apt-get install -y python3-distutils
+git clone --filter=blob:none --no-checkout https://github.com/kheiriddine/Models_Repositories/
+cd Models_Repositories
+git sparse-checkout init --cone
+git sparse-checkout set Models_Repositories  # Exclude /models/
+git checkout
