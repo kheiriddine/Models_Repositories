329479b3529b1cff8b3e5ff0dfaafc50385a3a0f
kheiriddine <111566229+kheiriddine@users.noreply.github.com>
2025-04-14 23:48:26 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,7 @@
 # Install system dependencies
 apt-get update && apt-get install -y python3-distutils git-lfs
 
+# Clone without LFS files
+GIT_LFS_SKIP_SMUDGE=1 git clone https://github.com/kheiriddine/Models_Repositories /mount/src/repo
+
 
